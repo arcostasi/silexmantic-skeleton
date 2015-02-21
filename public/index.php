11edd57b76d2f89607ab96d81aa8a80d16972ae5
Anderson Costa <arcostasi@gmail.com>
2015-02-21 10:21:41 -0200

    Add index

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<?php
+
+// Define Paths
+define('PATH_ROOT', dirname(__DIR__));
+define('PATH_CACHE', PATH_ROOT . '/data');
+define('PATH_LOG', PATH_CACHE . '/log');
+define('PATH_SRC', PATH_ROOT . '/src');
+define('PATH_CONFIG', PATH_ROOT . '/config');
+define('PATH_PUBLIC', PATH_ROOT . '/public');
+define('PATH_VENDOR', PATH_ROOT . '/vendor');
+define('PATH_VIEWS', PATH_ROOT . '/views');
+
+// Autoload
+require PATH_VENDOR . '/autoload.php';
+
+// App Init
+$app = new Silex\Application();
+
+// Development
+$app->run();
